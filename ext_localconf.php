0b7e293c755f0e0d00935b20cfc35015d3c288af
Oliver Hader <oliver@typo3.org>
2017-11-20 18:55:52 +0100

    [TASK] Change code formatting

--- ext_localconf.php
+++ ext_localconf.php
@@ -17,32 +17,31 @@ call_user_func(
             ]
         );
 
-    // wizards
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-        'mod {
-            wizards.newContentElement.wizardItems.plugins {
-                elements {
-                    information {
-                        iconIdentifier = car_rental-plugin-information
-                        title = LLL:EXT:car_rental/Resources/Private/Language/locallang_db.xlf:tx_car_rental_information.name
-                        description = LLL:EXT:car_rental/Resources/Private/Language/locallang_db.xlf:tx_car_rental_information.description
-                        tt_content_defValues {
-                            CType = list
-                            list_type = carrental_information
+        // wizards
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+            'mod {
+                wizards.newContentElement.wizardItems.plugins {
+                    elements {
+                        information {
+                            iconIdentifier = car_rental-plugin-information
+                            title = LLL:EXT:car_rental/Resources/Private/Language/locallang_db.xlf:tx_car_rental_information.name
+                            description = LLL:EXT:car_rental/Resources/Private/Language/locallang_db.xlf:tx_car_rental_information.description
+                            tt_content_defValues {
+                                CType = list
+                                list_type = carrental_information
+                            }
                         }
                     }
+                    show = *
                 }
-                show = *
-            }
-       }'
-    );
-		$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-		
-			$iconRegistry->registerIcon(
-				'car_rental-plugin-information',
-				\TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-				['source' => 'EXT:car_rental/Resources/Public/Icons/user_plugin_information.svg']
-			);
-		
+           }'
+        );
+
+        $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+        $iconRegistry->registerIcon(
+            'car_rental-plugin-information',
+            \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+            ['source' => 'EXT:car_rental/Resources/Public/Icons/user_plugin_information.svg']
+        );
     }
 );
