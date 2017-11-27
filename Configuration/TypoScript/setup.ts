93dfa0a26841428c50f30bbcb3f5bcabe89bf12a
Oliver Hader <oliver@typo3.org>
2017-11-27 20:01:55 +0100

    [TASK] Remove auto-generated CSS styles

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -45,37 +45,3 @@ plugin.tx_carrental_management {
         #callDefaultActionIfActionCantBeResolved = 1
     }
 }
-
-# these classes are only used in auto-generated templates
-plugin.tx_carrental._CSS_DEFAULT_STYLE (
-    textarea.f3-form-error {
-        background-color:#FF9F9F;
-        border: 1px #FF0000 solid;
-    }
-
-    input.f3-form-error {
-        background-color:#FF9F9F;
-        border: 1px #FF0000 solid;
-    }
-
-    .tx-car-rental table {
-        border-collapse:separate;
-        border-spacing:10px;
-    }
-
-    .tx-car-rental table th {
-        font-weight:bold;
-    }
-
-    .tx-car-rental table td {
-        vertical-align:top;
-    }
-
-    .typo3-messages .message-error {
-        color:red;
-    }
-
-    .typo3-messages .message-ok {
-        color:green;
-    }
-)
